8ceeab8d3553e19dff3448881223f0a42baddee5
Alexander Block <ablock84@gmail.com>
2018-11-24 10:24:06 +0100

    Change MAKEJOBS to -j3
    -j4 is too much for the Travis workers.

--- ci/matrix.sh
+++ ci/matrix.sh
@@ -28,7 +28,7 @@ export DOCKER_RUN_IN_BUILDER="docker run -t --rm -w $SRC_DIR $DOCKER_RUN_ARGS $B
 export GOAL="install"
 export SDK_URL=${SDK_URL:-https://bitcoincore.org/depends-sources/sdks}
 export PYTHON_DEBUG=1
-export MAKEJOBS="-j4"
+export MAKEJOBS="-j3"
 
 if [ "$BUILD_TARGET" = "arm-linux" ]; then
   export HOST=arm-linux-gnueabihf
